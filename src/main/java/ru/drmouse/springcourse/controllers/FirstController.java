86b8dd4c6fcaadf5109d8a2464ab9131134fffd4
Vadim <vadimshalin@mail.ru>
2022-05-17 14:38:06 +0300

    Continue

--- src/main/java/ru/drmouse/springcourse/controllers/FirstController.java
+++ src/main/java/ru/drmouse/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.drmouse.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +14,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -23,5 +26,36 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+
+        if (action == null) {
+            model.addAttribute("answer", "Incorrect param");
+        } else {
+            switch (action) {
+                case "multiplication":
+                    model.addAttribute("answer",a * b);
+                    break;
+                case "addition":
+                    model.addAttribute("answer",a + b);
+                    break;
+                case "subtraction":
+                    model.addAttribute("answer",a - b);
+                    break;
+                case "division":
+                    model.addAttribute("answer",a / b);
+                    break;
+                default:
+                    model.addAttribute("answer","Incorrect action");
+                    break;
+            }
+        }
+
+        return "first/calculator";
     }
 }
